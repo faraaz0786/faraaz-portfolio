8e8965caef39184ab29d90c78803972376a0f502
Faraaz <faraazkalim9@gmail.com>
2025-11-15 01:28:07 +0530

    Certifications Update

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,14 +1,17 @@
+import { label } from "framer-motion/client";
 import SectionHeader from "./SectionHeader";
 import { ExternalLink } from "lucide-react";
 
 export default function Certifications() {
   const certs = [
+    { label: "Data Science & Artificial Intelligence – IBM/Integral SkillsNetwork", url: "https://integral.skillsnetwork.site/certificates/dbd893e4-5b67-47eb-bcfd-3cca239fbd0d" },
     { label: "Data Fundamentals – IBM", url: "https://www.credly.com/go/iVBJ2r6q" },
     { label: "Cybersecurity Fundamentals – IBM SkillsBuild", url: "https://www.credly.com/go/SlSsn6OM" },
     { label: "Data Science 101 – IBM", url: "https://courses.integral.skillsnetwork.site/certificates/39ce0706d71947b487c38608c051abbb" },
     { label: "Digital Analytics & Regression – IBM", url: "https://courses.integral.skillsnetwork.site/certificates/5ac1a1618eb845379b88de4834cfb6e1" },
     { label: "Business Intelligence – IBM", url: "https://courses.integral.skillsnetwork.site/certificates/99d49e46bb1f4fc497986610f8661c1b" },
     { label: "Web Development Training – Internshala", url: "https://drive.google.com/file/d/1pfxZAP2KAgCrm0OYndpSmHKn5yfrxp1L/view?usp=drive_link" },
+    { label: "Machine Learning with Python – IBM/Integral SkillsNetwork", url: "https://courses.integral.skillsnetwork.site/certificates/ba9e8cb3365e4232b9dbda9e320f450d" },
   ];
 
   return (

2c16899752fbd9bdf863f42967ee69e04dbaf7fc
Faraaz <faraazkalim9@gmail.com>
2025-11-15 01:21:40 +0530

    Certifications Update

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -1,9 +1,11 @@
 export const CERTS = [
+    { title: "Data Science & Artificial Intelligence – IBM", issuer: "Integral University", url: "https://integral.skillsnetwork.site/certificates/dbd893e4-5b67-47eb-bcfd-3cca239fbd0d" },
     { title: "Data Fundamentals – IBM", issuer: "IBM", url: "https://www.credly.com/go/iVBJ2r6q" },
     { title: "Cybersecurity Fundamentals – IBM SkillsBuild", issuer: "IBM", url: "https://www.credly.com/go/SlSsn6OM" },
     { title: "Data Science 101 – IBM", issuer: "IBM Skills Network", url: "https://courses.integral.skillsnetwork.site/certificates/39ce0706d71947b487c38608c051abbb" },
     { title: "Digital Analytics & Regression – IBM", issuer: "IBM Skills Network", url: "https://courses.integral.skillsnetwork.site/certificates/5ac1a1618eb845379b88de4834cfb6e1" },
     { title: "Business Intelligence – IBM", issuer: "IBM Skills Network", url: "https://courses.integral.skillsnetwork.site/certificates/99d49e46bb1f4fc497986610f8661c1b" },
     { title: "Web Development Training – Internshala", issuer: "Internshala", url: "https://drive.google.com/file/d/1pfxZAP2KAgCrm0OYndpSmHKn5yfrxp1L/view?usp=drive_link" },
+    { title: "Machine Learning with Python – IBM", issuer: "Integral University", url: "https://courses.integral.skillsnetwork.site/certificates/ba9e8cb3365e4232b9dbda9e320f450d" },
   ]
   
